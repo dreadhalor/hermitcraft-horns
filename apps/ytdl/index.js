b8a8407a51226ebc7754a9c22b899b6e63fdf531
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-30 15:43:17 -0700

    Using ffmpeg to take a video slice

--- apps/ytdl/index.js
+++ apps/ytdl/index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
+const cp = require('child_process');
 const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg-static');
 
 const videoUrl = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
-const outputFilename = 'video.mp4';
+const outputFilename = 'video_slice.mp4';
+const timeStart = '00:00:10'; // Start time in HH:MM:SS format
+const timeDuration = '00:00:05'; // Duration in HH:MM:SS format
 
-async function downloadVideo() {
+async function downloadVideoSlice() {
   try {
     const videoInfo = await ytdl.getInfo(videoUrl);
     const format = ytdl.chooseFormat(videoInfo.formats, {
@@ -12,17 +16,36 @@ async function downloadVideo() {
       filter: 'audioandvideo',
     });
 
-    const videoStream = ytdl(videoUrl, { format, begin: '10s' });
-    const writeStream = fs.createWriteStream(outputFilename);
+    const videoStream = ytdl(videoUrl, { format });
+    const tempFilename = 'temp_video.mp4';
+    const tempWriteStream = fs.createWriteStream(tempFilename);
 
-    videoStream.pipe(writeStream);
+    videoStream.pipe(tempWriteStream);
 
-    writeStream.on('finish', () => {
-      console.log('Video downloaded successfully!');
+    tempWriteStream.on('finish', () => {
+      const ffmpegProcess = cp.spawn(ffmpeg, [
+        '-y',
+        '-v',
+        'error',
+        '-i',
+        tempFilename,
+        '-ss',
+        timeStart,
+        '-t',
+        timeDuration,
+        '-c',
+        'copy',
+        outputFilename,
+      ]);
+
+      ffmpegProcess.on('close', () => {
+        fs.unlinkSync(tempFilename); // Remove the temporary file
+        console.log(`Video slice saved to ${outputFilename}`);
+      });
     });
   } catch (error) {
     console.error('Error downloading video:', error);
   }
 }
 
-downloadVideo();
+downloadVideoSlice();
