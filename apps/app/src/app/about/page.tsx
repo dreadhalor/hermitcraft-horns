8a8fda62d74655d845f01c18e6ef28fc8344d92d
Dreadhalor <Scotthetrick2@yahoo.com>
2024-05-16 19:42:37 -0700

    Made the about page a lil better

--- apps/app/src/app/about/page.tsx
+++ apps/app/src/app/about/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import GoatHornImage from '@/assets/goat-horn.webp';
@@ -8,46 +7,51 @@ import Link from 'next/link';
 
 const AboutPage = () => {
   const router = useRouter();
-
   return (
-    <div className='about-page flex flex-1 flex-col gap-2 py-8'>
-      <div className='flex gap-2'>
+    <div className='about-page flex flex-1 flex-col gap-6 py-8'>
+      <div className='flex items-center gap-4'>
         <button
-          className='relative -mt-[2px] aspect-square h-[36px]'
+          className='relative -mt-[2px] aspect-square h-[48px] transition-transform duration-300 ease-in-out hover:scale-110'
           onClick={() => router.push('/')}
         >
           <Image src={GoatHornImage} alt='goat horn image' fill />
         </button>
-        <h1 className='text-3xl font-bold'>About</h1>
+        <h1 className='text-4xl font-bold text-gray-800'>About</h1>
       </div>
-      <p>
-        <b>Oh, snappers!</b> Welcome to Hermitcraft Horns! It means a lot to me
-        that you're here. 🎉
+      <p className='text-lg text-gray-700'>
+        <b className='text-xl text-blue-700'>Oh, snappers!</b> Welcome to
+        Hermitcraft Horns! It means a lot to me that you're here. 🎉
       </p>
-      <p>
+      <p className='text-lg text-gray-700'>
         As an avid viewer of Hermitcraft, I found myself seeing the same two
         sentiments echoed over and over in the comment sections of the hermits'
-        videos: "That would make a great horn!" & "What episode is that horn
-        from?"
+        videos: "That quote would make a great horn!" & "What episode is that
+        horn from?"
       </p>
-      <p>
+      <p className='text-lg text-gray-700'>
         & since there isn't a place to easily create & share those moments, I
-        made one!
+        made one everyone can use for free!
       </p>
-      <p>
+      <p className='text-lg text-gray-700'>
         Here, you can easily load up any YouTube video in the&nbsp;
-        <Link href='/create' className='underline'>
+        <Link
+          href='/create'
+          className='text-blue-700 underline hover:text-blue-800'
+        >
           Create
         </Link>
         &nbsp;tab & create as many downloadable horns of all of your favorite
         Hermit quotes as you want, along with viewing others' favorite quotes
         too! Download as many as you want & use them in your own servers!
       </p>
-      <p>
+      <p className='text-lg text-gray-700'>
         On top of that, you can find the context of any horn by simply clicking
         'View clip' on a horn & the source video will be automatically loaded
         into the{' '}
-        <Link href='/create' className='underline'>
+        <Link
+          href='/create'
+          className='text-blue-700 underline hover:text-blue-800'
+        >
           Create
         </Link>{' '}
         tab with the clip already set up for you so you won't have to wonder
@@ -55,34 +59,36 @@ const AboutPage = () => {
         short, an episode or a full stream, you can easily find the context of
         any horn!
       </p>
-      <p>
+      <p className='text-lg text-gray-700'>
         & of course, this is just the initial release of the site. There will be
-        many optimizations & additional features added as our horn collection
-        gets bigger, so this is just the beginning.
+        many optimizations & additional features added to make the site faster &
+        more functional as our horn collection gets bigger, so this is just the
+        beginning.
       </p>
-      <p>
+      <p className='text-lg text-gray-700'>
         The code is open source so if you're a coder, feel free to check out the{' '}
         <a
           href='https://github.com/dreadhalor/hermitcraft-horns'
           target='_blank'
           rel='noreferrer'
-          className='underline'
+          className='text-blue-700 underline hover:text-blue-800'
         >
           Hermitcraft Horns GitHub repo
         </a>{' '}
         & give it a star! 🌟
       </p>
-      <p>
-        If you have any suggestions, feedback, or issues, feel free to reach out
-        to me on Twitter at{' '}
+      <p className='text-lg text-gray-700'>
+        I don't use socia media much but if you have any suggestions, feedback,
+        or issues, you can reach out to my Reddit account at{' '}
         <a
-          href='https://twitter.com/scotthetrick'
+          href='https://www.reddit.com/user/dreadhalor/'
           target='_blank'
           rel='noreferrer'
-          className='underline'
+          className='text-blue-700 underline hover:text-blue-800'
         >
-          @scotthetrick
+          @dreadhalor
         </a>
+        .
       </p>
     </div>
   );
