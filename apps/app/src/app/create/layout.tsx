0321b7c597848a70f683a1d0706083515f3f8a3d
Dreadhalor <Scotthetrick2@yahoo.com>
2024-05-23 02:56:21 -0700

    Pivoting this into a different branch

--- apps/app/src/app/create/layout.tsx
+++ apps/app/src/app/create/layout.tsx
@@ -1,13 +1,8 @@
 import { ClipBuilderProvider } from '@/providers/clip-builder-provider';
 import React from 'react';
-import { AudioProvider } from '@repo/audio-editor';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ClipBuilderProvider>
-      <AudioProvider>{children}</AudioProvider>
-    </ClipBuilderProvider>
-  );
+  return <ClipBuilderProvider>{children}</ClipBuilderProvider>;
 };
 
 export default Layout;
