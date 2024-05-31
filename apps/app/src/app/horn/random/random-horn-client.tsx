ace5597c0aa8b65391a5f467ef18ea762258111b
Dreadhalor <Scotthetrick2@yahoo.com>
2024-05-31 16:51:29 -0700

    Added "randomize horn" button

--- apps/app/src/app/horn/random/random-horn-client.tsx
+++ apps/app/src/app/horn/random/random-horn-client.tsx
@@ -4,10 +4,12 @@ import { HornTile } from '@/components/horn-tile/horn-tile';
 import { Horn } from '@/trpc';
 import { Button } from '@ui/button';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useRef } from 'react';
 
 const RandomHornClient = ({ horn }: { horn: Horn }) => {
   const hornRef = useRef<any>();
+  const router = useRouter();
 
   return (
     <>
@@ -19,6 +21,14 @@ const RandomHornClient = ({ horn }: { horn: Horn }) => {
         }}
       >
         Play / Stop
+      </Button>
+      <Button
+        className='-mt-1 w-full'
+        onClick={() => {
+          router.refresh();
+        }}
+      >
+        Randomize Horn
       </Button>
       <Link href='/home' className='mt-2 hover:underline'>
         &larr; Back to home
