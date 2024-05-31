03ff60949d185c03c27fc9ad898c1fea837c5fa8
Dreadhalor <Scotthetrick2@yahoo.com>
2024-05-30 18:00:29 -0700

    Hard-coding this is because the imageRef breaks in prod

--- apps/app/src/components/site-menu/site-menu.tsx
+++ apps/app/src/components/site-menu/site-menu.tsx
@@ -10,23 +10,23 @@ interface Props {
   imageRef: React.RefObject<HTMLImageElement>;
 }
 export const SiteMenu = ({ imageRef }: Props) => {
-  const [top, setTop] = React.useState(0);
-  React.useLayoutEffect(() => {
-    setTop((imageRef.current?.getBoundingClientRect().height ?? 0) / 2);
-  }, [imageRef]);
+  // const [top, setTop] = React.useState();
+  // React.useLayoutEffect(() => {
+  //   setTop((imageRef.current?.getBoundingClientRect().height ?? 0) / 2);
+  // }, [imageRef]);
 
-  console.log('top', top);
+  // console.log('top', top);
 
-  if (!top) return null;
+  // if (!top) return null;
 
   return (
     <Sheet>
       <SheetTrigger asChild>
         <Button
-          className='pointer-events-auto sticky right-0 top-4 z-10 ml-auto h-10 w-10 rounded-full bg-[#3554A9] p-0 shadow-md hover:bg-[#354B87]'
-          style={{
-            marginTop: `${top + 5}px`,
-          }}
+          className='pointer-events-auto sticky right-0 top-[70px] z-10 ml-auto h-10 w-10 rounded-full bg-[#3554A9] p-0 shadow-md hover:bg-[#354B87] sm:top-[90px]'
+          // style={{
+          //   marginTop: `${50}px`,
+          // }}
         >
           <IoMenu />
         </Button>
